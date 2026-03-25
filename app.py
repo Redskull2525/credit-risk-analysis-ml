41f4144a01c3433309eb65006942cfa880d99241
Abhishek Shelke <86852008+Redskull2525@users.noreply.github.com>
2026-03-25 15:37:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,8 +68,7 @@ if st.button("🔍 Predict Risk"):
     # Match training columns
     input_data = input_data.reindex(columns=log_model.feature_names_in_, fill_value=0)
 
-    # Select model
-    model_choice == "Logistic Regression"
+    #prediction
     prediction = log_model.predict(input_data)
     prob = log_model.predict_proba(input_data)[0][1]
 
